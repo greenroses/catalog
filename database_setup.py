640fdbee84e6e9d0399060883baf19eb1a6d44b6
greenroses <xiaodan.zju@gmail.com>
2018-01-19 08:56:09 +0000

    Fix: now able to add description longer than 250 chars

--- database_setup.py
+++ database_setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2.7
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -41,7 +41,7 @@ class Item(Base):
 
     title = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
-    description = Column(String(250))
+    description = Column(Text)
     category_id = Column(Integer, ForeignKey('category.id'))
     timestamp = Column(DateTime(timezone=True), server_default=func.now())
     category = relationship(Category)
